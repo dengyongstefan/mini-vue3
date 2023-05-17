34471ca3d97fc767fb8ad51657c398d39e12f0d3
Deng Yong <1060756423@qq.com>
2023-05-17 15:53:17 +0800

    tag: patch element

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1,5 +1,6 @@
 import { ShapeFlags } from 'packages/shared/src/shapeFlags';
 import { Comment, Fragment, Text } from './vnode'
+import { EMPTY_OBJ } from '@vue/shared';
 
 /**
  * @description 对外暴露创建渲染器
@@ -26,23 +27,25 @@ function baseCreateRenderer(options:RenderOptions):any{
         createElement: hostCreateElement,
     } = options
 
-    // 处理元素
+    // element 处理元素
     const processElement = (oldVNode, newVNode, container, anchor)=>{
         if(oldVNode == null){
             // 没有旧节点直接执行挂载操作
             mountElement(newVNode, container, anchor)
         }else{
             // 更新操作
+            patchElement(oldVNode,newVNode)
         }
     }
 
-    // 挂载元素节点
+    // 挂载元素节点 vnode会挂载el
     const mountElement = (vnode, container, anchor) => {
         const { type, props, shapeFlag } = vnode
         // 1. 创建element
         const el = vnode.el = hostCreateElement(type)
         // 2. 设置文本
         if(shapeFlag & ShapeFlags.TEXT_CHILDREN){
+            // 是文本节点
             hostSetElementText(el, vnode.children)
         }else{
             //todo
@@ -55,6 +58,95 @@ function baseCreateRenderer(options:RenderOptions):any{
         }
         // 4. 插入DOM
         hostInsert(el, container, anchor)
+    }
+
+    // 更新节点
+    const patchElement = (oldVNode, newVNode) => {
+        // 获取el
+        const el = (newVNode.el = oldVNode.el)
+
+        // 获取新旧的props值
+        const oldProps = oldVNode.props || EMPTY_OBJ
+        const newProps = newVNode.props || EMPTY_OBJ
+
+        // patch 子节点
+        patchChildren(oldVNode,newVNode,el,null)
+
+        // patch props
+        patchProps(el,newVNode, oldProps,newProps )
+    }
+
+    // 为子节点打补丁
+    const patchChildren = (oldVNode, newVNode, container, anchor) => {
+        // 旧节点的 children
+        const c1 = oldVNode && oldVNode.children
+        // 旧节点的 ShapeFlag
+        const prevShapeFlag = oldVNode ? oldVNode.shapeFlag : 0
+        // 新节点的 children
+        const c2 = newVNode.children
+        // 新节点的 shapeFlag
+        const newShapeFlag = newVNode.shapeFlag
+
+        // 新子节点为 TEXT_CHILDREN
+        if(newShapeFlag & ShapeFlags.TEXT_CHILDREN){
+             // 旧子节点为 ARRAY_CHILDREN
+            if(prevShapeFlag & ShapeFlags.ARRAY_CHILDREN){
+                // todo 卸载旧的子节点
+            }
+            // 新旧子节点不同
+            if(c2 != c1){
+                // 挂载新子节点的文本
+                hostSetElementText(container,c2)
+            }
+
+        }else{
+        // 新子节点不是 TEXT_CHILDREN
+            // 旧子节点为 ARRAY_CHILDREN
+            if(prevShapeFlag & ShapeFlags.ARRAY_CHILDREN){
+                // 新子节点也为 ARRAY_CHILDREN
+                if(newShapeFlag & ShapeFlags.ARRAY_CHILDREN){
+                    // 进行diff
+                }
+                else{
+                    // 新子节点不为 ARRAY_CHILDREN，则直接卸载旧子节点
+                    // todo 卸载旧的子节点
+                }
+            }else{
+                // 旧子节点为 TEXT_CHILDREN
+                if(prevShapeFlag & ShapeFlags.TEXT_CHILDREN){
+                    // 删除旧节点
+                    hostSetElementText(container,'')
+                }
+                // 新子节点为 ARRAY_CHILDREN
+                if(newShapeFlag & ShapeFlags.ARRAY_CHILDREN){
+                    // 单独挂载子节点操作
+                }
+            }
+        }
+    }
+
+    // 为props打补丁
+    const patchProps = (el: Element, vnode, oldProps, newProps) => {
+        // 新旧 props 不相同时才进行处理
+        if(oldProps !==  newProps){
+              // 遍历新的 props，依次触发 hostPatchProp ，赋值新属性
+            for (const key in newProps) {
+                const next = newProps[key]
+                const prev = oldProps[key]
+                if(next != prev){
+                    hostPatchProp(el,key,prev,next)
+                }
+            }
+        }
+        // 存在旧的 props 时
+        if(oldProps !== EMPTY_OBJ){
+              // 遍历旧的 props，依次触发 hostPatchProp ，删除不存在于新props 中的旧属性
+            for (const key in oldProps) {
+                if(!(key in newProps)){
+                    hostPatchProp(el,key,oldProps[key],null)
+                }
+            }
+        }
     }
 
     /**
@@ -81,7 +173,7 @@ function baseCreateRenderer(options:RenderOptions):any{
                     // 普通DOM元素
                     processElement(oldValue, newValue, container, anchor)
                 }else if(shapeFlag & ShapeFlags.COMPONENT){
-                    // 组件
+                    // todo 组件
 
                 }
         }
